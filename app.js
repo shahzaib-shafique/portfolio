33a7e3917a0d19f30525e99cbce8c2342ad363c9
shahzaib-shafique <shahzaibshafique734@gmail.com>
2025-09-04 09:20:12 -0700

    portfolio

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var typed = new Typed('#element', {
-  strings: [
-    'Front-End Developer',
-    'Web Designer'
-  ],
-  typeSpeed: 60,       // typing speed
-  backSpeed: 40,       // backspacing speed
-  backDelay: 1500,     // pause before backspacing
-  startDelay: 500,     // delay before typing starts
-  loop: true,          // keep looping
-  showCursor: true,    // show blinking cursor
-  cursorChar: '|',     // customize cursor symbol
+  strings: ['Front-End Developer', 'Web Designer'],
+  typeSpeed: 50,
+  backSpeed: 35,
+  backDelay: 1000,
+  startDelay: 500,
+  loop: true,
+  showCursor: true,
+  cursorChar: '|',
   smartBackspace: true
 });
