751f594fb9eeac7b68f80e49790908afb2942c0e
shahzaib-shafique <shahzaibshafique734@gmail.com>
2025-09-08 00:09:42 -0700

    my portfolio

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var typed = new Typed('#element', {
   strings: ['Front-End Developer', 'Web Designer'],
   typeSpeed: 50,
@@ -9,3 +10,42 @@ var typed = new Typed('#element', {
   cursorChar: '|',
   smartBackspace: true
 });
+
+
+const sections = document.querySelectorAll("main section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+
+
+const hamburger = document.getElementById("hamburger");
+const navLinksContainer = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinksContainer.classList.toggle("show");
+});
+/*portfolio */
